e1038008dc4f2bf18b3e0569fee90ecf2f7e4d40
Hermes Jimenez Luna <ermez3@gmail.com>
2020-07-28 22:20:08 -0500

    issue-16 Se corrigen nombres de columnas. Esto para evitar tener nombres de columnas repetidas.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycfdi_transform",
-    version="0.0.0.4",
+    version="0.0.0.5",
     author="SW sapien",
     author_email="dev@sw.com.mx",
     description="Cfdi Xml Transformation column format/csv",
