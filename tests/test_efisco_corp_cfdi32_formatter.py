f88d5d8a01b9de53cea4d99e5423fe241d16e66d
Francisco Balderrama <francisco.balderrama@sw.com.mx>
2022-02-14 16:22:36 -0600

    EFISCO-599 correct naming in cfdi32 tests

--- tests/test_efisco_corp_cfdi32_formatter.py
+++ tests/test_efisco_corp_cfdi32_formatter.py
@@ -1,4 +1,3 @@
-from pycfdi_transform import CFDI33SAXHandler
 from pycfdi_transform.formatters.cfdi32.efisco_cfdi32_formatter import EfiscoCorpCFDI32Formatter
 import unittest
 
@@ -7,7 +6,7 @@ from pycfdi_transform.sax.cfdi32.sax_handler import CFDI32SAXHandler
 
 class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
 
-    def test_columns_names_cfdi33(self):
+    def test_columns_names_cfdi32(self):
         formatter = EfiscoCorpCFDI32Formatter({'cfdi32': {}})
         columns_expected = [
             'VERSION', 
@@ -51,7 +50,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
             'SELLOCFD']
         self.assertListEqual(columns_expected, formatter.get_columns_names())
     
-    def test_initialize_class_error_cfdi33(self):
+    def test_initialize_class_error_cfdi32(self):
         with self.assertRaises(AssertionError) as ex:
             EfiscoCorpCFDI32Formatter(cfdi_data=None)
         self.assertEqual('El Formatter debe recibir el objeto devuelto por la transformación el debe ser un dict.', str(ex.exception))
@@ -61,7 +60,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
             EfiscoCorpCFDI32Formatter({'cfdi33': {}})
         self.assertEqual('Este formatter únicamente soporta datos de cfdi32.', str(ex.exception))
 
-    def test_formatter_error_tfd_cfdi33(self):
+    def test_formatter_error_tfd_cfdi32(self):
         sax_handler = CFDI32SAXHandler()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         cfdi_data.pop('tfd10')
@@ -69,7 +68,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
         self.assertFalse(formatter.can_format())
         self.assertEqual(formatter.get_errors(), 'Not tfd11 in data.')
     
-    def test_formatter_cfdi33_implocal10_ok(self):
+    def test_formatter_cfdi32_implocal10_ok(self):
         sax_handler = CFDI32SAXHandler().use_implocal10()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         formatter = EfiscoCorpCFDI32Formatter(cfdi_data)
@@ -79,7 +78,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
         self.assertTrue(len(data_columns) == 2)
         self.assertTrue(len(data_columns[0]) == len(formatter.get_columns_names()))
     
-    def test_formatter_cfdi33_implocal10_safe_numerics_1(self):
+    def test_formatter_cfdi32_implocal10_safe_numerics_1(self):
         sax_handler = CFDI32SAXHandler().use_implocal10()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         formatter = EfiscoCorpCFDI32Formatter(cfdi_data,empty_char='',safe_numerics=True)
@@ -90,7 +89,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
         self.assertTrue(len(data_columns[0]) == len(formatter.get_columns_names()))
         self.assertEqual(data_columns[0][32],'0.00')
     
-    def test_formatter_cfdi33_implocal10_safe_empty_char_1(self):
+    def test_formatter_cfdi32_implocal10_safe_empty_char_1(self):
         sax_handler = CFDI32SAXHandler().use_implocal10()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         formatter = EfiscoCorpCFDI32Formatter(cfdi_data,empty_char='-',safe_numerics=False)
@@ -101,7 +100,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
         self.assertTrue(len(data_columns[0]) == len(formatter.get_columns_names()))
         self.assertEqual(data_columns[0][32],'-')
     
-    def test_formatter_cfdi33_implocal10_multiple_complements(self):
+    def test_formatter_cfdi32_implocal10_multiple_complements(self):
         sax_handler = CFDI32SAXHandler().use_implocal10()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         formatter = EfiscoCorpCFDI32Formatter(cfdi_data,empty_char='-',safe_numerics=False)
@@ -113,7 +112,7 @@ class TestEfiscoCorpCFDI32Formatter(unittest.TestCase):
         self.assertEqual(data_columns[0][32],'-')
         self.assertEqual(data_columns[0][33],'-')
     
-    def test_formatter_line_breaks_cfdi33(self):
+    def test_formatter_line_breaks_cfdi32(self):
         sax_handler = CFDI32SAXHandler().use_concepts_cfdi32()
         cfdi_data = sax_handler.transform_from_file('./tests/Resources/cfdi32/cfdi32_01.xml')
         formatter = EfiscoCorpCFDI32Formatter(cfdi_data)
