74c12bd2baa19bbc828faf24e0fd4572a0e9ce6c
Koronos <christian.verdia@sw.com.mx>
2022-02-17 11:59:54 -0600

    EFISCO-599
    - Pruebas actualizadas para coincidir con las relaciones de complementos

--- pycfdi_transform/formatters/pagos10/efisco_pagos10_formatter.py
+++ pycfdi_transform/formatters/pagos10/efisco_pagos10_formatter.py
@@ -6,7 +6,7 @@ from pycfdi_transform.helpers.string_helper import StringHelper
 class EfiscoPagos10Formatter(FormatterInterface):
     def __init__(self, cfdi_data: dict, empty_char: str = '', safe_numerics: bool = False) -> EfiscoPagos10Formatter:
         super().__init__(cfdi_data, empty_char, safe_numerics)
-        assert 'cfdi33' in self._cfdi_data, 'este formatter solo soporta datos de cfdi33.'
+        assert 'cfdi33' in self._cfdi_data, 'Este formatter solo soporta datos de cfdi33.'
 
     @staticmethod
     def _get_id_pago(count_complement: int, count_pago: int, count_dr: int) -> str:
