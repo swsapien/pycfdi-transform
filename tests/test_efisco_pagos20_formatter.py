2a5f8fdf7b8528b7f969e6b97df7086837fa2ae7
Francisco Balderrama <francisco.balderrama@sw.com.mx>
2022-03-02 11:42:05 -0600

    EFISCO-599 fix in formater columns with pagos22 UT fixes

--- tests/test_efisco_pagos20_formatter.py
+++ tests/test_efisco_pagos20_formatter.py
@@ -56,7 +56,8 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
             "P_DR_SERIE",
             "P_DR_FOLIO",
             "P_DR_MONEDADR",
-            "P_DR_EQUIVALENCIADR",
+            "P_DR_TIPOCAMBIODR",
+            "P_DR_METODODEPAGODR",
             "P_DR_NUMPARCIALIDAD",
             "P_DR_IMPSALDOANT",
             "P_DR_IMPPAGADO",
@@ -101,10 +102,9 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
         self.assertEqual(len(row_list), 1, 'No rows returned.')
         for row in row_list:
             self.assertEqual(len(row), len(formatter.get_columns_names()), 'Different length of columns')
-        first_row = ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P', '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6', 'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '1', '200.00', '200.00', '0.00']
+        first_row = ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P', '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6', 'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '', '1', '200.00', '200.00', '0.00']
         self.assertListEqual(first_row, row_list[0])
 
- #DONE
     def test_rows_pagos20_01_empty_char(self):
         start = time.time()
         sax_handler = CFDI40SAXHandler().use_pagos20()
@@ -115,12 +115,11 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
         self.assertEqual(formatter.get_errors(), '', 'Errors obtained.')
         row_list = formatter.dict_to_columns()
         end_time = start - time.time()
-        print(row_list[0])
-        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '-', '0.0', 'XXX', '-', 'P', '-', '-', '-', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '-', '14000.00', '-', '-', '-', '-', '-', '-', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '1', '5000.00', '2000.00', '3000.00']
+        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '-', '0.0', 'XXX', '-', 'P', '-', '-', '-', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '-', '14000.00', '-', '-', '-', '-', '-', '-', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '', '1', '5000.00', '2000.00', '3000.00']
         self.assertListEqual(first_row, row_list[0])
         self.assertLess(end_time, 0.01)
 
-    #DONE
+
     def test_rows_pagos20_01_safe_numerics(self):
         start = time.time()
         sax_handler = CFDI40SAXHandler().use_pagos20()
@@ -132,7 +131,7 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
         row_list = formatter.dict_to_columns()
         end_time = start - time.time()
         print(row_list[0])
-        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '0.00', '0.0', 'XXX', '1.00', 'P', '', '', '', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '1.00', '14000.00', '', '', '', '', '', '', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '1', '5000.00', '2000.00', '3000.00']
+        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '0.00', '0.0', 'XXX', '1.00', 'P', '', '', '', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '1.00', '14000.00', '', '', '', '', '', '', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '', '1', '5000.00', '2000.00', '3000.00']
         self.assertListEqual(first_row, row_list[0])
         self.assertLess(end_time, 0.01)
 
@@ -146,8 +145,7 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
         self.assertEqual(formatter.get_errors(), '', 'Errors obtained.')
         row_list = formatter.dict_to_columns()
         end_time = start - time.time()
-        print(row_list[0])
-        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '0.00', '0.0', 'XXX', '1.00', 'P', '-', '-', '-', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '1.00', '14000.00', '-', '-', '-', '-', '-', '-', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '1', '5000.00', '2000.00', '3000.00']
+        first_row = ['4.0', 'Serie', 'Folio', '2021-12-16T15:40:21', '30001000000400002444', '0', '0.00', '0.0', 'XXX', '1.00', 'P', '-', '-', '-', '20008', 'XAXX010101000', 'PUBLICO GENERAL', '601', 'XAXX010101000', 'Nombre', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-02T00:18:10', '01', 'USD', '1.00', '14000.00', '-', '-', '-', '-', '-', '-', '1488703.81', '0.00', '2.00', '0.00', '0.00', 'BEDC8964-7E57-4604-9968-7E01378E8706', 'Serie3', 'Folio3', 'MXN', '1.329310', '', '1', '5000.00', '2000.00', '3000.00']
         self.assertListEqual(first_row, row_list[0])
         self.assertLess(end_time, 0.01)
 
@@ -163,21 +161,9 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
         for row in row_list:
             self.assertEqual(len(row), len(formatter.get_columns_names()), 'Different length of columns')
         expected_rows = [
-            ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P',
-             '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6',
-             'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24',
-             '2022-02-08T21:58:56', 'SPR190613I52',
-             'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==',
-             'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '',
-             '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '1', '200.00', '200.00', '0.00'],
-            ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P',
-             '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6',
-             'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24',
-             '2022-02-08T21:58:56', 'SPR190613I52',
-             'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==',
-             'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '',
-             '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '1', '200.00', '200.00', '0.00']
-        ]
+                ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P', '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6', 'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '', '1', '200.00', '200.00', '0.00'],
+                ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P', '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6', 'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '080CF54E-F9DB-4470-BF0D-A8613DC10B24', '2022-02-08T21:58:56', 'SPR190613I52', 'WUCP5ykONZrtAg97dDf4bx/GIldE0diCw1LDmCUci3YI31ocsvAQJCRTrWJ9JNkr3UrD1CyO//vxhP65DvmuNGyK0+QrQn7FDvEX+vo0bslbMP+UprBevco4JYW3BLazIpU1rSmoiu1K0ViAZCpdRk+o13uV8be4SQGOPclLsTljUxcYdn2qHLlP+EhSyqEb7MNUTQvY44MpjPlRRfgHnhDVQZDGZyta1M5jHrQioIl625Ju6PFDNMMyt+VltFb9omWpa7QxWuIaQ/jpmtWxqVGZjjpNj6aT8g1gjnr+dqyBQ5FpRunDfj9YgQJAs5Dug4S6Mwzj474egKjLKYlUoQ==', 'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '', '1', '200.00', '200.00', '0.00']
+            ]
         self.assertListEqual(expected_rows, row_list)
     
     def test_rows_pagos20_complete(self):
@@ -196,12 +182,6 @@ class TestEfiscoPagos10Formatter(unittest.TestCase):
             self.assertEqual(len(row), len(formatter.get_columns_names()), 'Different length of columns')
             print(row)
         expected_rows = [
-            ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P',
-             '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6',
-             'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '9D81C696-0401-4F85-B703-6E0D3AFD6057',
-             '2019-03-29T17:42:38', 'AAA010101AAA',
-             'G3knPza1r8hfxSb+46JBRAYoQIUTVU2Uqlw/qiMLbzKkqrUs2zs5GVRjf0DJEm5mIGZIC3/+q/FdU1A4zr9RCXWM66QmtK7AAlkT4HSrX9NlCoJylwRzRW6bxuchfy8ryFmWIetAOU2U1brWLSSYqCtc86m4iIBId6kDOSIr+/Tpz2Q3rDHhmtlJyJ2ovy1rjLO0mZpbwhwOKg6Mo1+NeEuGk4UfQu8gfRgv9mOnbiNkfiJHzvuRJnUXYrL/9D1gDQ6gjW8t75W/yYN+Gf2TmpyTnOkYZbFqrJ8udt7rZjXsODLapD7cV4bjsLIYxOWHgXEIgrTjB57m2POrfb72wQ==',
-             'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '',
-             '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '1', '200.00', '200.00', '0.00']
-        ]
+            ['4.0', 'Serie', 'Folio', '2022-02-08T00:18:10', '30001000000400002434', '0', '', '0.00', 'XXX', '', 'P', '', '', '', '20000', 'EKU9003173C9', 'ESCUELA KEMPER URGATE SA DE CV', '601', 'URE180429TM6', 'UNIVERSIDAD ROBOTICA ESPAÑOLA SA DE CV', 'G01', '9D81C696-0401-4F85-B703-6E0D3AFD6057', '2019-03-29T17:42:38', 'AAA010101AAA', 'G3knPza1r8hfxSb+46JBRAYoQIUTVU2Uqlw/qiMLbzKkqrUs2zs5GVRjf0DJEm5mIGZIC3/+q/FdU1A4zr9RCXWM66QmtK7AAlkT4HSrX9NlCoJylwRzRW6bxuchfy8ryFmWIetAOU2U1brWLSSYqCtc86m4iIBId6kDOSIr+/Tpz2Q3rDHhmtlJyJ2ovy1rjLO0mZpbwhwOKg6Mo1+NeEuGk4UfQu8gfRgv9mOnbiNkfiJHzvuRJnUXYrL/9D1gDQ6gjW8t75W/yYN+Gf2TmpyTnOkYZbFqrJ8udt7rZjXsODLapD7cV4bjsLIYxOWHgXEIgrTjB57m2POrfb72wQ==', 'CP1_P1_DR1', '2021-12-15T00:00:00', '01', 'MXN', '1', '200.00', '', '', '', '', '', '', '', '', '', '', '', '', '', 'bfc36522-4b8e-45c4-8f14-d11b289f9eb7', '', '', 'MXN', '1', '', '1', '200.00', '200.00', '0.00']
+                    ]
         self.assertListEqual(expected_rows, row_list)
