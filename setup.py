182759b48b4e4b36ceaaeed69a25104651fd26cc
Francisco Balderrama <francisco.balderrama@sw.com.mx>
2022-03-03 15:10:22 -0600

    EFISCO-599 up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycfdi_transform",
-    version="0.1.2.5-rc1",
+    version="0.1.2.6-rc1",
     author="SW sapien",
     description="Cfdi Xml Transformation column format/csv",
     long_description=long_description,
