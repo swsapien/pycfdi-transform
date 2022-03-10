b6e519997150b46770189e9982d8b744b41bd428
saul.martinez <saul.martinez@sw.com.mx>
2022-03-10 10:26:30 -0600

    EFISCO-631  - updated lib version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycfdi_transform",
-    version="0.1.2.9-rc4",
+    version="0.1.2.10",
     author="SW sapien",
     description="Cfdi Xml Transformation column format/csv",
     long_description=long_description,
