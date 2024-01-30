fc26f2ed4c863c4df0559c07aa4b7f9fd77a1c63
Koronos <christian.verdia@sw.com.mx>
2024-01-30 15:35:30 -0600

    - corrección de versión

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycfdi_transform",
-    version="0.1.8.0",
+    version="0.1.10.0",
     author="SW sapien",
     description="Cfdi Xml Transformation column format/csv",
     long_description=long_description,
